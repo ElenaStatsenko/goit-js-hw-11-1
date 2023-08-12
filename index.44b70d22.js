d6922d4cc997b4500aa8b5964d3a651eed304190
ElenaStatsenko <118728440+ElenaStatsenko@users.noreply.github.com>
2023-08-12 06:28:04 +0000

    Deploying to gh-pages from @ ElenaStatsenko/goit-js-hw-11-1@f0e2641e537047d3de84d925ac9f02fd03e1a06f 🚀

---
+++ index.44b70d22.js
@@ -0,0 +1,2 @@
+fetch("https://pixabay.com/api/?key=38261227-21424640981700fd9c4931e5f&image_type=photo&orientation=horizontal&safesearch=true").then((e=>e.json())).then(console.log);
+//# sourceMappingURL=index.44b70d22.js.map
