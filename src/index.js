92f9c20f883fc20abdc6013632d62ec43cc1ec36
Mykhailo Statsenko <118728440+ElenaStatsenko@users.noreply.github.com>
2023-08-20 18:32:21 +0300

    мамйже зробила

--- src/index.js
+++ src/index.js
@@ -3,76 +3,111 @@ import Notiflix from 'notiflix';
 import NewsApiService from './news-api-service';
 
 const refs = {
-    searchForm: document.querySelector('.search-form'),
-    loadMoreBtn: document.querySelector('.load-more'),
-    divGallery: document.querySelector('.gallery'),
-}
+  searchForm: document.querySelector('.search-form'),
+  loadMoreBtn: document.querySelector('.load-more'),
+  divGallery: document.querySelector('.gallery'),
+};
 const newsApiService = new NewsApiService();
 
 refs.searchForm.addEventListener('submit', onSearch);
 refs.loadMoreBtn.addEventListener('click', onLoadMore);
+refs.loadMoreBtn.style.display = 'none';
 
-function onSearch (e) {
-    e.preventDefault();
-   
-    newsApiService.query = e.currentTarget.elements.searchQuery.value;
-    if(newsApiService.query==='') {
-      return onErorr()
-    } else {
-      Notiflix.Notify.info(`Hooray! We found ${imageHit.totalHits} images.`);
-    }
-    newsApiService.resetPage();
+function onSearch(e) {
+  e.preventDefault();
+  newsApiService.query = e.currentTarget.elements.searchQuery.value;
+  if (newsApiService.query === '') {
+    return onErorr();
+  }
+  newsApiService.resetPage();
 
-    newsApiService.fetchArticles()
+  // первый запрос на сервер
+  newsApiService
+    .fetchArticles()
     .then(data => renderArticles(data))
     .then(data => {
-       articlesContainer();
-      appendArticklesMarkup(data)});
+      // console.log(data.length);
+      // console.log(newsApiService.totalHits);
+
+      // if (data.length === newsApiService.totalHits) {
+      //   // refs.loadMoreBtn.style.display = 'none';
+      //   Notiflix.Notify.info(
+      //     `We're sorry, but you've reached the end of search results.`
+      //   );
+      // }
+
+      Notiflix.Notify.info(
+        `Hooray! We found ${newsApiService.totalHits} images.`
+      );
+      articlesContainer();
+      appendArticklesMarkup(data);
+      refs.loadMoreBtn.style.display = 'block';
+    });
 }
 
+// Повторный запрос на сервер на кнопке
 function onLoadMore(e) {
-    newsApiService.fetchArticles()
-    .then(data => renderArticles(data))
-    .then(appendArticklesMarkup);  
+  newsApiService
+    .fetchArticles()
+    .then(data => {
+      console.log(data.length);
+      console.log(newsApiService.totalHits);
+
+      if (data.length === newsApiService.totalHits) {
+        // refs.loadMoreBtn.style.display = 'none';
+        Notiflix.Notify.info(
+          `We're sorry, but you've reached the end of search results.`
+        );
+      }
+
+      return renderArticles(data);
+    })
+    .then(data => appendArticklesMarkup(data));
 }
 
-let imageHit = '';
-
-// прописіваем шаблон для разметки
+// прописываем шаблон для разметки
 function renderArticles(hits) {
-    const imageHit = hits.map(hit => `
+  const imageHit = hits.map(
+    hit =>
+      `
      
-    /* <div class="photo-card">
+     <div class="photo-card">
     <img src="${hit.largeImageURL}" alt="${hit.tags}" width=400px loading="lazy" />
     <div class="info">
       <p class="info-item">
-        <b>${hit.likes}</b>
+        <b>likes:${hit.likes}</b>
       </p>
       <p class="info-item">
-        <b>${hit.views}</b>
+        <b>Views:${hit.views}</b>
       </p>
       <p class="info-item">
-        <b>${hit.comments}</b>
+        <b>Comments: ${hit.comments}</b>
       </p>
       <p class="info-item">
-        <b>${hit.downloads}</b>
+        <b>Downloads:${hit.downloads}</b>
       </p>
     </div>
-  </div> */
+  </div> 
      `
-      );
+  );
 
-      return imageHit;
+  return imageHit;
 }
 // рендер разметки
 function appendArticklesMarkup(text) {
-    refs.divGallery.innerHTML = text;
-    
+  refs.divGallery.innerHTML = text;
 }
 // очищаем контейнер с картинками при новом запросе
 function articlesContainer() {
   refs.divGallery.innerHTML = '';
 }
 function onErorr() {
-  Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+  Notiflix.Notify.failure(
+    'Sorry, there are no images matching your search query. Please try again.'
+  );
+}
+
+// сравниваем кол-во загруженых картинок с  тоталхитс
+function loadQuantity() {
+ 
 }
--- src/news-api-service.js
+++ src/news-api-service.js
@@ -1,34 +1,41 @@
-export default class NewsApiService{
-    constructor(){
-        // в конструкторе задаем значение глобальной переменной
-        this.searchQuery = '';
-        this.page = 1;
-    }
-    fetchArticles(){
-        console.log('до запроса', this)
-        const url =`https://pixabay.com/api/?q=${this.searchQuery}&key=38261227-21424640981700fd9c4931e5f&per_page=40&page=${this.page}&image_type=photo&orientation=horizontal&safesearch=true`;
+export default class NewsApiService {
+  constructor() {
+    // в конструкторе задаем значение глобальной переменной
+    this.searchQuery = '';
+    this.page = 1;
+    this.totalHits = 0;
+  }
 
-       return fetch(url)
-        .then(response =>response.json())
-        .then(data =>{
-            this.incrementPage();
-            return data.hits;
-        });
-        }
-        // увеличиваем страницу на 1 при каждом успешном запросе
-        incrementPage() {
-            this.page += 1;
-        }
-        // сбрасываем страничку при новом запросе
-        resetPage() {
-            this.page = 1;
-        }
-        // добавлем геттер
-    get query() {
-        return this.searchQuery;
-    }
-    // перезаписываем значение сеттером
- set query(newQuery) {
-    this.searchQuery= newQuery;
- }
+  fetchArticles() {
+    // console.log('до запроса', this);
+    const url = `https://pixabay.com/api/?q=${this.searchQuery}&key=38261227-21424640981700fd9c4931e5f&per_page=40&page=${this.page}&image_type=photo&orientation=horizontal&safesearch=true`;
+
+    return fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        this.addTotalHits(data);
+        this.incrementPage();
+        return data.hits;
+      });
+  }
+  // увеличиваем страницу на 1 при каждом успешном запросе
+  incrementPage() {
+    this.page += 1;
+  }
+  // сбрасываем страничку при новом запросе
+  resetPage() {
+    this.page = 1;
+  }
+  // добавлем геттер
+  get query() {
+    return this.searchQuery;
+  }
+  // перезаписываем значение сеттером
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+
+  addTotalHits(data) {
+    return (this.totalHits = data.totalHits);
+  }
 }
